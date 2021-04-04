327bb94587606536e6d1d9769fe0c369ff1a16f2
Dave <davedabyeet@gmail.com>
2021-04-04 18:37:48 -0500

    Ready to deploy

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body::-webkit-scrollbar {
+  width: 0.5rem;
+}
+body::-webkit-scrollbar-thumb {
+  background-color: rebeccapurple;
+}
+html {
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  scrollbar-track-color: azure;
+}
+* {
+  font-size: inherit;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
     @media (max-width: 768px) {
     html{
         font-size:50%;
