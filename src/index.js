17531ae2fbd5f0c9e43da3dbac149bd83aed53f5
Dave <davedabyeet@gmail.com>
2021-04-05 11:45:37 -0500

    Https issues fixed, minor tweaks left

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -1,13 +1,13 @@
 export const getWeather = async (place) => {
   const apikey = "Ncv3Cd8WB4lovhpkVj13zZ6uJRibtDIN";
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${apikey}&q=${place}`;
   var res = await fetch(base + query);
   const data = await res.json();
   var key = typeof data[0] !== "undefined" ? data[0].Key : "328328";
 
   async function fetchweather() {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${key}?apikey=${apikey}`;
 
     var response = await fetch(base + query);
