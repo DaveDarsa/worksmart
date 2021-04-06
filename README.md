b6d32399a73709468ff5ff7ccea64adb72b4d1c2
Dave <davedabyeet@gmail.com>
2021-04-05 23:40:58 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-#worksmart yay
--- src/utils/smoothscroll.js
+++
@@ -1,8 +0,0 @@
-export function smoothscroll(elem) {
-  var element = document.getElementById(elem);
-  element.scrollIntoView({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-}
